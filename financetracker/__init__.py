a402d9a08f0124b15993f378662850839af877e8
Mikes987 <misk@posteo.de>
2025-03-19 11:20:14 +0100

    Adding Config Class

--- financetracker/__init__.py
+++ financetracker/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
+from .config import Config
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY='no-secret-now'
-    )
+    app.config.from_object(config_class)
     
     from financetracker.main import main_bp
     app.register_blueprint(main_bp)
