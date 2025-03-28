672c4b65dc4dc63880e6ef8553bd2f7781bbbb40
Mikes987 <misk@posteo.de>
2025-03-28 11:05:51 +0100

    Currencies integrated into database

--- financetracker/main/routes.py
+++ financetracker/main/routes.py
@@ -1,5 +1,5 @@
 from financetracker.main import main_bp as bp
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_login import current_user
 
 
@@ -7,3 +7,11 @@ from flask_login import current_user
 @bp.route('/index')
 def index():
     return render_template('index.html', title='Index')
+
+
+@bp.route("/user/<username>")
+def usersettings(username: str):
+    if current_user.is_anonymous:
+        print("No redirect!")
+        return redirect(url_for('main.index'))
+    return render_template('usersettings.html', title="User Settings")
--- financetracker/models.py
+++ financetracker/models.py
@@ -18,6 +18,8 @@ class User(db.Model, UserMixin):
     email: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True)
     password_hash: so.Mapped[str] = so.mapped_column(sa.String(256))
     
+    user_categories: so.WriteOnlyMapped['Category'] = so.relationship(back_populates='user')
+    
     def __repr__(self):
         return f'<User: "{self.username}">'
     
@@ -60,3 +62,89 @@ class User(db.Model, UserMixin):
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
+
+
+class MainTypes(db.Model):
+    __tablename__ = 'maintypes'
+    
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    type: so.Mapped[str] = so.mapped_column(sa.String(32), unique=True)
+    
+    categories: so.WriteOnlyMapped['Category'] = so.relationship(back_populates='main_type')
+    
+    def __repr__(self):
+        return f"<Type: {self.type}>"
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    category: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True, index=True)
+    main_type_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('maintypes.id'))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('users.id'))
+    
+    main_type: so.Mapped['MainTypes'] = so.relationship(back_populates='categories')
+    user: so.Mapped['User'] = so.relationship(back_populates='user_categories')
+    
+    def __repr__(self):
+        return f'<Category: "{self.category}">'
+
+
+class CurrencyExchanges(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    exchange_value: so.Mapped[float]
+    currency_to_dollar: so.Mapped[str] = so.mapped_column(sa.String(3))
+    
+    def __repr__(self):
+        return f"<Currency Exchange_to_Dollar ({self.currency_to_dollar}: {self.exchange_value})"
+    
+    @staticmethod
+    def update_exchange_rates(content: dict):
+        keys = content.keys()
+        for key in keys:
+            exchange_rate = db.session.scalar(sa.select(CurrencyExchanges).where(CurrencyExchanges.currency_to_dollar==key))
+            if exchange_rate == None:
+                print("Creating new value for currency " + key)
+                exchange_rate = CurrencyExchanges(currency_to_dollar=key)
+            exchange_rate.exchange_value = content[key]
+            db.session.add(exchange_rate)
+        db.session.commit()
+
+
+class  CurrencyUpdateStatus(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    last_update_unix: so.Mapped[int]
+    last_update_string: so.Mapped[str] = so.mapped_column(sa.String(24))
+    next_update_unix: so.Mapped[int]
+    next_update_string: so.Mapped[str] = so.mapped_column(sa.String(24))
+    
+    def __repr__(self):
+        return f'<CurrencyUpdateStatus (last update: {self.last_update_string})>'
+    
+    @staticmethod
+    def update(content):
+        if content['result'] != 'success':
+            print("Load from API not successful")
+        else:
+            last_status = db.session.scalar(sa.select(CurrencyUpdateStatus).where(CurrencyUpdateStatus.id==1))
+            if last_status == None:
+                print("Creating initial status object")
+                last_status = CurrencyUpdateStatus()
+            else:
+                if last_status.last_update_unix >= content['time_last_update_unix']:
+                    print("No updates yet")
+                    return
+            print("Updates available")
+            last_update = content['time_last_update_unix']
+            last_update_string = datetime.strftime((datetime.fromtimestamp(last_update)), format='%y-%m-%d %H:%M:%S')
+            next_update = content['time_next_update_unix']
+            next_update_string = datetime.strftime((datetime.fromtimestamp(next_update)), format='%y-%m-%d %H:%M:%S')
+            last_status.last_update_unix = last_update
+            last_status.last_update_string = last_update_string
+            last_status.next_update_unix = next_update
+            last_status.next_update_string = next_update_string
+            db.session.add(last_status)
+            print("Adding currency exchanges")
+            CurrencyExchanges.update_exchange_rates(content['rates'])
+            
