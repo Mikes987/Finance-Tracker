f7130723c9081f5db1a843f3c6e554dc7e636d71
Mikes987 <misk@posteo.de>
2025-03-31 12:00:09 +0200

    config file extended with defaults

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    ADMINS = os.environ.get('ADMINS')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "Not-so-secret-key-in-config-file"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT')) or 8025
+    ADMINS = os.environ.get('ADMINS') or ['your-email@example.com']
